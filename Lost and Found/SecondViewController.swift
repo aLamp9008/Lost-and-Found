8dab3702bb2c7c8c89f238157a7400478dbac004
Alden Lamp <alamp9008@gmail.com>
2016-04-02 17:58:23 -0400

    Initial Commit

---
+++ Lost and Found/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Lost and Found
+//
+//  Created by alden lamp on 4/2/16.
+//  Copyright © 2016 JasonAldenVincent. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
