3b88353b2f2078f7fa905b3440b7793da0387582
Alden Lamp <alamp9008@gmail.com>
2016-04-03 07:15:53 -0400

    making the obj-c file

---
+++ Lost and Found/-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  -Bridging-Header.h
+//  Lost and Found
+//
+//  Created by alden lamp on 4/3/16.
+//  Copyright © 2016 JasonAldenVincent. All rights reserved.
+//
+#import "TwilioCloent.h"
+
+#ifndef _Bridging_Header_h
+#define _Bridging_Header_h
+
+
+#endif /* _Bridging_Header_h */
---
+++ Lost and Found/Phone.swift
@@ -0,0 +1,16 @@
+//
+//  Phone.swift
+//  Lost and Found
+//
+//  Created by alden lamp on 4/3/16.
+//  Copyright © 2016 JasonAldenVincent. All rights reserved.
+//
+
+import Foundation
+
+public class Phone {
+    
+    var device: TCDevice? = nil;
+    var connection: TCConnection? = nil;
+    
+}
